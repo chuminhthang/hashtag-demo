9706447c275830ed86496d988539cd09f1bdcda6
chu.minh.thang <chu.minh.thang@rewardstyle.com>
2020-11-27 14:13:32 +0700

    Fix some UI

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -6,12 +6,9 @@
           <font size="2" style="font-weight: bold">UPLOAD IMAGE </font><br />
           <font size="1">Any solution, It's free </font>
         </div>
-        <!-- <div id="sub-middle">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-right-circle" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                <path fill-rule="evenodd" d="M4 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5A.5.5 0 0 0 4 8z"/>
-            </svg>
-        </div> -->
+        <div id="sub-middle">
+            <div class="circle">></div>
+        </div>
         <div id="sub-right" :style="{'color': uploadSeen ? '#bf9d9d' : 'black'}">
           <font size="2" style="font-weight: bold">SEE RESULT </font><br />
           <font size="1">See all item detected </font>
@@ -94,8 +91,8 @@
 <script>
 import Result from "./Result.vue";
 
-// const IMAGE_PROCESSING_API = 'http://192.168.0.32:8000/api/upload_image/'
-const IMAGE_PROCESSING_API = "http://192.168.0.32:5000/search";
+const IMAGE_PROCESSING_API = 'https://7f3t4ql73k.execute-api.us-east-1.amazonaws.com/prod/ria-ai-hashtag-lambda-1'
+// const IMAGE_PROCESSING_API = "http://192.168.0.32:5000/search";
 const IMAGE_PROCESSING_URL = "http://192.168.0.32:5000"
 // const IMAGE_PROCESSING_API = 'http://localhost:5000/'
 
